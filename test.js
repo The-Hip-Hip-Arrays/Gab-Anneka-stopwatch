3bb6568cf630ca1a5c1f63b47aa7cb35c1c80419
Gabriel Perales <gabrielperales@gmail.com>
2016-06-15 11:15:27 +0100

    firsts tests

--- test.js
+++ test.js
@@ -1,9 +1,44 @@
+var stopwatch;
+
+stopwatch = {
+  elapsedtime: 0,
+  start: function(){
+    setInterval(function(){
+      stopwatch.elapsedtime = 10;
+    }, 10);
+  }
+};
 
 
-/* ------------- Q-UNIT ------------- */
-/* ------------- TESTS ------------- */
-
-test( 'Check that tests are working, firstTest returns true', function(assert) {
-  var actual = firstTest();
-  assert.ok(actual, 'true === true' );
+test('testing the testing environment', function(assert) {
+  var actual = 1;
+  var expected = 1;
+  assert.equal(actual, expected, 'test environment is set up correctly');
 });
+
+test('A function constructor for stopwatch should exist', function(assert){
+  assert.ok(typeof stopwatch === 'object');
+});
+
+test('A start method should exist within the object', function(assert) {
+  assert.ok(typeof stopwatch.start === 'function');
+});
+
+test('at the beginning elapsedtime should be 0', function(assert) {
+  var expected = 0;
+  assert.equal(stopwatch.elapsedtime, expected, 'Elapsed time is set to 0s before start begins');
+});
+
+test('start method should update the elapsedtime property of the stopwatch each 10ms', function(assert){
+  var done = assert.async();
+  stopwatch.start();
+  var expected = 10;
+  setTimeout(function() {
+    assert.equal(stopwatch.elapsedtime, expected, 'Elapsed 10ms after start the stopwatch');
+    done();
+  }, 10);
+});
+
+// test('after 2 ticks of 10ms, elapsedtime of the stopwatch should be 20', function(assert){
+//
+// });
