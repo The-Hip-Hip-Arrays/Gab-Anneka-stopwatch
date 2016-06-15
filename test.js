b4a93de3bdc71fa77eb5cad95f09cdde3d5de796
Shadows666 <lifeloveregretsxe@gmail.com>
2016-06-15 10:28:46 +0100

    set up initial files

---
+++ test.js
@@ -0,0 +1,9 @@
+
+
+/* ------------- Q-UNIT ------------- */
+/* ------------- TESTS ------------- */
+
+test( 'Check that tests are working, firstTest returns true', function(assert) {
+  var actual = firstTest();
+  assert.ok(actual, 'true === true' );
+});
